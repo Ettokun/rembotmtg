42d125325d2b0d019324cbb276734d5568538d86
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-02 11:54:07 +0100

    Update bypassnitro.js

--- commands/bypassnitro.js
+++ commands/bypassnitro.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js")
 
 exports.run = (client, message, args) => {
   const emoji = message.guild.emojis.find("name", args[0])
- //const emoji1 = message.guild.emojis.find("name", args[1])
+ const emoji1 = message.guild.emojis.find("name", args[1])
   
   if(!emoji) return message.channel.send("I can't")
   if(!emoji.animated) return message.channel.send("This emoji is not animated") 
   
-  /*if(args[0] === "noembed") {
+  if(args[0] === "noembed") {
     message.delete()
     message.channel.send(`<a:${emoji1.name}:${emoji1.id}>`)
-  }*/
+  }
   
   message.delete()
   const embed = new Discord.MessageEmbed().setTitle(message.author.username+"#"+message.author.discriminator)
