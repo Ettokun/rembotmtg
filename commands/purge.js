4a115bae367a5090404f4bbe054a927f570ca1a2
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-02 11:52:31 +0100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -5,10 +5,10 @@ exports.run = async (client, message, args) => {
   message.delete()
   var argTo = args[0]
   const member = message.mentions.members.first() || message.guild.members.get(argTo)
-  /*if(member) {
+  if(member) {
     if(!args[1]) return message.channel.send(":x: Your argument is not a number\n```\nUsage: r!purge [@member] | <id> <amount>\nExample: r!purge @MTG 50\n\nDelete a lot of messages.```")
     argTo = args[1]
-  }*/
+  }
   if(!isNaN(argTo)) {
     amount = argTo
   } else {
@@ -16,13 +16,13 @@ exports.run = async (client, message, args) => {
   }
 
   
- /* message.channel.bulkdelete(argTo).then(c => {
+  message.channel.bulkdelete(argTo).then(c => {
     c.send(`Purged ${argTo-1} message(s)`).then(m => {
       setTimeout(() => {
         m.delete()
       }, 4000) 
     })
-  }) */
+  }) 
  
   
    message.channel.messages.fetch({
