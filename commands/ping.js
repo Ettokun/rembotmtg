e9fcca870ab8557ab9d2fa7f337326e0079ff23d
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-02 11:52:52 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,13 +6,13 @@ exports.run = (client, message, args) => {
      .setDescription(`[\`${Math.round(client.ws.ping)}ms\`](https://google.com)`)
    message.channel.send(ping)
 
-/*const embed = new Discord.RichEmbed() 
+const embed = new Discord.RichEmbed() 
 .setTitle("Bot: Wolf Management#9627 Author: [WSD]One K.#5237") 
 .setThumbnail("https://cdn.discordapp.com/avatars/505428551722336257/82464b97205d2b18a9551475e890f667.png?size=1024")
 .setColor('BLACK')
 .addField(":star: 7.17/10", '"Good bot in general, but it not really friendly user."')  
 
-message.channel.send(embed)*/
+message.channel.send(embed)
 } 
 
 exports.help = {
